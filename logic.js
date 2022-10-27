aa110e46303a6a2e9c9dce9abe399425addcded0
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-10-27 18:22:08 +0100

    Made local variables global to prevent variable shadowing

--- logic.js
+++ logic.js
@@ -1,20 +1,20 @@
+let playerChoice
+let computerChoice
+let playerScore = 0
+let computerScore = 0
+
 game()
 
 function game () {
 
-    let playerChoice
-    let computerChoice
-    let playerScore = 0
-    let computerScore = 0
-
     for (let looper = 0; looper < 5; looper++) {
         playerChoice = prompt("Enter rock, paper or scissors").toLowerCase()
         computerChoice = (getComputerChoice())
-        console.log(playRound(playerChoice, computerChoice, playerScore, computerScore))
-        console.log(displayScore(playerScore, computerScore))
+        console.log(playRound())
+        console.log(displayScore())
      }
 
-    console.log(determineWinner(playerScore, computerScore)) 
+    console.log(determineWinner()) 
 }
 
 function getComputerChoice() {    
@@ -32,25 +32,25 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerChoice, computerChoice, playerScore, computerScore) {
+function playRound() {
 
     switch (playerChoice) {
     case "rock":
             
         switch (computerChoice) {
         case "rock":
-                return "Both chose rock, it's a tie."
-                break
+            return "Both chose rock, it's a tie."
+            break
     
         case "paper":
-                return "Paper beats rock, you lose!"
-                computerScore++
-                break
+            computerScore++
+            return "Paper beats rock, you lose!"
+            break
 
         case "scissors":
-                return "Rock beats scissors, you win"
-                playerScore++
-                break
+            playerScore++
+            return "Rock beats scissors, you win"
+            break
         }
 
     break
@@ -59,18 +59,18 @@ function playRound(playerChoice, computerChoice, playerScore, computerScore) {
 
         switch (computerChoice) {
         case "rock":
-                return "Paper beat's rock, you win!"
-                playerScore++
-                break
+            playerScore++
+            return "Paper beat's rock, you win!"
+            break
     
         case "paper":
-                return "Both chose paper, it's a tie."
-                break
+            return "Both chose paper, it's a tie."
+            break
 
         case "scissors":
-                return "Scissors beats paper, you lose!"
-                computerScore++
-                break
+            computerScore++
+            return "Scissors beats paper, you lose!"
+            break
         }
 
     break
@@ -79,29 +79,29 @@ function playRound(playerChoice, computerChoice, playerScore, computerScore) {
 
         switch (computerChoice) {
         case "rock":
-                return "Rock beat's scissors, you lose!"
-                computerScore++
-                break
+            computerScore++
+            return "Rock beat's scissors, you lose!" 
+            break
     
-        case "paper":
-                return "Scissors beats paper, you win!"
-                playerScore++
-                break
+        case "paper": 
+            playerScore++
+            return "Scissors beats paper, you win!"
+            break
 
         case "scissors":
-                return "Both chose scissors, it's a tie."
-                break
+            return "Both chose scissors, it's a tie."
+            break
         }
 
     break
     }
 }
 
-function displayScore(playerScore, computerScore) {
+function displayScore() {
     return ("Score:\n" + "Player " + playerScore + "-" + computerScore + " Computer")
 }
 
-function determineWinner (playerScore, computerScore) {
+function determineWinner () {
     if (playerScore > computerScore) {
         return "The Player wins the game!"
     } else if (computerScore > playerScore) {
