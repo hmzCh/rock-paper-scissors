4c6c55d2c8ec3b3146d21f26a6d26eb8174f50e7
Hamza <114156862+hmzCh@users.noreply.github.com>
2022-10-27 16:51:01 +0100

    Add function to compare choices to determine winner

--- logic.js
+++ logic.js
@@ -4,18 +4,80 @@ let playerChoice,
 playerChoice = prompt("Enter rock, paper or scissors").toLowerCase()
 computerChoice = (getComputerChoice())
 
-function getComputerChoice() {
-    let choice
+playRound(playerChoice, computerChoice)
+
+
+
+function getComputerChoice() {    
     
-switch (Math.floor(Math.random() * 3)) {
-    case 0:
-        return "rock"
-        break
-    case 1:
-        return "paper"
-        break
-    case 2:
-        return "scissors"
-        break
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock"
+            break
+        case 1:
+            return "paper"
+            break
+        case 2:
+            return "scissors"
+            break
+    }
 }
+
+function playRound(playerChoice, computerChoice) {
+
+    switch (playerChoice) {
+    case "rock":
+            
+        switch (computerChoice) {
+        case "rock":
+                console.log("Both chose rock, it's a tie.")
+                break
+    
+        case "paper":
+                console.log("Paper beats rock, you lose!")
+                break
+
+        case "scissors":
+                console.log("Rock beats scissors, you win")
+                break
+        }
+
+    break
+        
+    case "paper":
+
+        switch (computerChoice) {
+        case "rock":
+                console.log("Paper beat's rock, you win!")
+                break
+    
+        case "paper":
+                console.log("Both chose paper, it's a tie.")
+                break
+
+        case "scissors":
+                console.log("Scissors beats paper, you lose!")
+                break
+        }
+
+    break
+
+    case "scissors":
+
+        switch (computerChoice) {
+        case "rock":
+                console.log("Rock beat's scissors, you lose!")
+                break
+    
+        case "paper":
+                console.log("Scissors beats paper, you win!")
+                break
+
+        case "scissors":
+                console.log("Both chose scissors, it's a tie.")
+                break
+        }
+
+    break
+    }
 }
